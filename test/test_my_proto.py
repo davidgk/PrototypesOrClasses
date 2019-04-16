dbec0eed2350d148a667cbeaf9fa4c554cce16b2
David Kotlirevsky <david.kotlirevsky@gmail.com>
2019-04-16 13:00:17 -0300

    Improve test names

--- test/test_my_proto.py
+++ test/test_my_proto.py
@@ -6,7 +6,7 @@ from model.car import Car
 
 class TestPrototyping(TestCase):
 
-    def test_when_create_a_car_then_prototype_it_should_be_pretty_similar(self):
+    def test_when_create_a_delegate_from_prototype_then_both_should_behave_same_way(self):
         my_car_prototype = Car("super_movil", "blue")
         creator = PrototypeCreator(my_car_prototype)
         my_car_delegate = creator.get()
@@ -15,7 +15,7 @@ class TestPrototyping(TestCase):
         self.assertEqual(my_car_prototype.brand(), my_car_delegate.brand())
         self.assertEqual(my_car_prototype.color(), my_car_delegate.color())
 
-    def test_create_a_prototype_from_car_and_replace_color(self):
+    def test_when_delegate_change_some_behave_then_prototype_should_not(self):
         my_car_prototype = Car("super_movil", "blue")
         replacer = (lambda: "red")
         creator = PrototypeCreator(my_car_prototype, {"color": replacer})
@@ -26,7 +26,7 @@ class TestPrototyping(TestCase):
         self.assertEqual(my_car_delegate.color(), "red")
         self.assertEqual(my_car_prototype.brand(), my_car_delegate.brand())
 
-    def test_create_a_prototype_remove_func(self):
+    def test_when_delegate_remove_some_behave_should_answer_with_prototype_behave(self):
         my_car_prototype = Car("super_movil", "blue")
         replacer = (lambda: "green")
         creator = PrototypeCreator(my_car_prototype, {"color": replacer})
